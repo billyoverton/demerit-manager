77b8e6026727eb727d84e57d11c0050641a73ad0
Billy Overton <overton.billy@gmail.com>
2013-11-14 20:55:08 -0500

    Database schema. First Pass.

---
+++ schema.sql
@@ -0,0 +1,33 @@
+drop table if exists users;
+create table users (
+    id integer primary key autoincrement,
+    name text not null,
+    registration_id text not null,
+    public_key BLOB not null
+);
+
+drop table if exists decrees;
+create table decrees (
+    id text primary key,
+    text text not null,
+    date text not null,
+    creator integer not null
+);
+
+drop table if exists decree_signatures;
+create table decree_signatures (
+    signer integer not null,
+    decree integer not null,
+    signature blob not null
+);
+
+drop table if exists demerits;
+create table demerits (
+    issuer integer not null,
+    receiver integer not null,
+    reason text not null,
+    date text not null,
+    issuer_signature blob not null,
+    receiver_signature blob
+);
+
